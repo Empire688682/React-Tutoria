9ed4aff12deb32f9a145f56f2230c56ddd17b8eb
Empire688682 <asehindej@gmail.com>
2024-01-13 21:58:33 +0100

    somes files updated

--- src/Component/Myapp.jsx
+++ src/Component/Myapp.jsx
@@ -1,40 +1,15 @@
-import React, { useReducer} from 'react'
+import React, {useRef} from 'react'
 import '../Component/Myapp.css';
+import Button from './button'
 
 function Myapp() {
 
-  const reducer = (state, action) =>{
-    switch (action.type) {
-      case "INCREMENT":
-        return{count: state.count + 1, showText: state.showText}
-      case "toggleShowText":
-        return{count: state.count, showText: !state.showText}
-      default:
-       return state;
-    }
-  }
+  const buttonRef = useRef(null)
 
-  const [state, dispatch] = useReducer(reducer, {count: 0, showText:true });
-
-  const doSomething = () =>{
-    dispatch({type: "INCREMENT"});
-    if(state.count %2===1){
-      dispatch({type: "toggleShowText"});
-    }else{
-      !dispatch({type: "toggleShowText"});
-    }
-    if(state.count === 10){
-      state.count = 0;
-    }
-
-  }
   return ( 
     <div className='container'>
-    <h1>{state.count}</h1>
-    <button onClick={doSomething}>Increment</button>
-    <div>
-    {state.showText && "ShowText"}
-    </div>
+      <button onClick={() =>{buttonRef.current.alterToggle()}}>From parent</button>
+      <Button ref={buttonRef}/>
     </div>
   )
 }
